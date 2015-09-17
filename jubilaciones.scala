fecbdb4881921d2fccb00948aff474be732bfc52
judithfc <js19tl@gmail.com>
2015-09-17 18:51:10 -0500

    Ejercicios 3

--- jubilaciones.scala
+++ jubilaciones.scala
@@ -1,15 +1,15 @@
-
-println("para saber en que tipo de jubilacion sera ingrese su edad")
+println("Para saber en que tipo de jubilacion sera, ingrese su edad: ")
 val edad=readFloat()
 
-println("Ingrese la antiguedad en su empleo")
+println("Ingrese la antiguedad en su empleo: ")
 val anti=readFloat()
 
-if(edad >= 60 anti < 25 ){
-println("La jubilación es por EDAD")}
 
-if (edad < 60 anti >= 25){
-println("La jubilación es por ANTIGUEDAD JOVEN")}
+if ((edad >=60)&&(anti <25)){
+println("La jubilacion es por EDAD")}
 
-if (edad >= 60 anti >= 60){
-println("La jubilación es por ANTIGUEDAD ADULTA")}
+if ((edad < 60)&&(anti >=25)){
+println("La antiguedad es por ANTIGUEDAD JOVEN")}
+
+if ((edad >=60)&&(anti >=25)){
+println("La jubilacion es por ANTIGUEDAD ADULTA")}
