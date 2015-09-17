ed224cd82d3f6730c36b1001e58c25f35e1bdf78
judithfc <js19tl@gmail.com>
2015-09-16 20:31:53 -0500

    Ejercicios02
    ya corregi el PR, gracias Profe.

---
+++ salario.scala
@@ -0,0 +1,21 @@
+println("ingrese el numero de horas trabajadas")
+
+val hrsn = readFloat()
+
+if(hrsn <=40) 
+{
+val ss = hrsn*160
+println("el salario semanal es de: "+ss)
+
+
+} 
+
+if (hrsn >40)
+{
+val ss=(hrsn*160)+200
+
+println("el salario semanal es de: "+ss)
+}
+
+
+
